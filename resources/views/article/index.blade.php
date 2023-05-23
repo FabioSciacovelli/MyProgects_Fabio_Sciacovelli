84efb4ab42c4cf5451560cee987bbd7070d22911
Valeriaciav <valeria.ciavola@gmail.com>
2023-05-23 21:24:08 +0200

    aggiunto bottone scopri di piuù in altre pagine, carosello e sezione categorie, testing

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -16,11 +16,20 @@
                     </div>
                     <div class="card-footer text-muted d-flex justify-content-center align-items-center">
                         Redatto il {{$article->created_at->format('d/m/Y')}} da  <a href="{{route('article.byUser', ['user' => $article->user->id])}}" class="small text-muted fst-italic text-capitalize m-2">{{$article->user->name}}</a>
-                        
+                        <a href="{{route('article.show', compact('article'))}}" class="ms-5 btn btn-success text-white">Leggi</a>
                     </div>
                 </div>            
             </div>
             @endforeach
+            
+        </div>
+        
+    </div>
+    <div class="container mb-5">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-2">
+                <a href="{{route ('homepage')}}" class="btn btn-success text-white ">Torna indietro</a>
+            </div>
         </div>
     </div>
 </x-layout>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,41 @@
         </div>
     @endif
 
+  <div class="container">
+    <div class="row m-3">
+       <div class="col-12 justify-content-end">
+           <a href="{{route('article.create')}}" type="submit" class="btn btn-success">Inserisci il tuo articolo</a>
+       </div>
+    </div>
+  </div>
+
+  <div class="container my-5">
+    <div class="row justify-content-around">
+        @foreach($articles as $article)
+        <div class="col-12 col-md-6 mt-4">
+            <div class="card shadow">
+                <img src="{{Storage::url($article->image)}}" class="card-img-top" alt="">
+                <div class="card-body">
+                  <h5 class="card-title">{{$article->title}}</h5>
+                  <p class="card-text">{{$article->subtitle}}</p>
+                  <p class="small text-muted fst-italic text-capitalize">
+                    <a href="{{route('article.byCategory', ['category' => $article->category->id])}}" class="small text-muted fst-italic text-capitalize">{{$article->category->name}}</a>
+                </p>
+                </div>
+                <div class="card-footer text-muted d-flex justify-content-center align-items-center">
+                    Redatto il {{$article->created_at->format('d/m/Y')}} da  <a href="{{route('article.byUser', ['user' => $article->user->id])}}" class="small text-muted fst-italic text-capitalize m-2">{{$article->user->name}}</a>
+                    <a href="{{route('article.show', compact('article'))}}" class="ms-5 btn btn-success text-white">Leggi</a> 
+                </div>
+
+            </div>            
+        </div>
+        @endforeach
+    </div>
+</div>
+
     <div class="container my-5">
         <div class="row justify-content-center">
-            <div class="col-12 col-md-5">
+            <div class="col-12">
                 <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
                     <div class="carousel-indicators">
                       <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -16,21 +48,21 @@
                     </div>
                     <div class="carousel-inner">
                       <div class="carousel-item active">
-                        <img src="" class="d-block w-100" alt="foto di switch">
+                        <img src="/media/carro-sello1.jpg" class="d-block w-100" alt="foto di switch">
                         <div class="carousel-caption d-none d-md-block">
                           <h5>First slide label</h5>
                           <p>Some representative placeholder content for the first slide.</p>
                         </div>
                       </div>
                       <div class="carousel-item">
-                        <img src="sfondogreen_presto.png" class="d-block w-100" alt="...">
+                        <img src="/media/carro-sello2.jpg" class="d-block w-100" alt="...">
                         <div class="carousel-caption d-none d-md-block">
                           <h5>Second slide label</h5>
                           <p>Some representative placeholder content for the second slide.</p>
                         </div>
                       </div>
                       <div class="carousel-item">
-                        <img src="sfondogreen_presto.png" class="d-block w-100" alt="...">
+                        <img src="/media/carro-sello3.jpg" class="d-block w-100" alt="...">
                         <div class="carousel-caption d-none d-md-block">
                           <h5>Third slide label</h5>
                           <p>Some representative placeholder content for the third slide.</p>
@@ -51,32 +83,53 @@
         </div>
     </div>
 
-    <div class="d-flex justify-content-center m-3">
-        <a href="{{route('article.create')}}" type="submit" class="btn btn-success">Inserisci il tuo articolo</a>
-    </div>
+
+
+   
 
     <div class="container my-5">
-        <div class="row justify-content-around">
-            @foreach($articles as $article)
-            <div class="col-12 col-md-5 mt-4">
-                <div class="card shadow">
-                    <img src="{{Storage::url($article->image)}}" class="card-img-top" alt="">
-                    <div class="card-body">
-                      <h5 class="card-title">{{$article->title}}</h5>
-                      <p class="card-text">{{$article->subtitle}}</p>
-                      <p class="small text-muted fst-italic text-capitalize">
-                        <a href="{{route('article.byCategory', ['category' => $article->category->id])}}" class="small text-muted fst-italic text-capitalize">{{$article->category->name}}</a>
-                    </p>
-                    </div>
-                    <div class="card-footer text-muted d-flex justify-content-center align-items-center">
-                        Redatto il {{$article->created_at->format('d/m/Y')}} da  <a href="{{route('article.byUser', ['user' => $article->user->id])}}" class="small text-muted fst-italic text-capitalize m-2">{{$article->user->name}}</a>
-                        
-                    </div>
-                </div>            
-            </div>
-            @endforeach
+      <div class="row mb-3">
+        <div class="col-12 col-md-4">
+          <div class="card">
+            <img src="/media/palazzoverde.jpg" class="card-img-top" alt="...">
+            <a href=""></a>
+          </div>
         </div>
+
+        <div class="col-12 col-md-4">
+          <div class="card">
+            <img src="/media/energia.jpg" class="card-img-top" alt="...">
+          </div>
+        </div>
+
+        <div class="col-12 col-md-4">
+          <div class="card">
+            <img src="/media/inquinamento-ambientale.jpg" class="card-img-top" alt="...">
+          </div>
+        </div>
+      </div>
+
+      <div class="row mb-5">
+        <div class="col-12 col-md-4">
+          <div class="card">
+            <img src="/media/eco-consigli.jpg" class="card-img-top" alt="...">
+          </div>
+        </div>
+
+        <div class="col-12 col-md-4">
+          <div class="card">
+            <img src="/media/economia-circolare.jpg" class="card-img-top" alt="...">
+          </div>
+        </div>
+
+        <div class="col-12 col-md-4">
+          <div class="card">
+            <img src="/media/curiosita.jpg" class="card-img-top" alt="...">
+          </div>
+        </div>
+      </div>
     </div>
+
 </x-layout>
                 
 
