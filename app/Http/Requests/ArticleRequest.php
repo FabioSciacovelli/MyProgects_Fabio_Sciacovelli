12278807a2d0e1183a3a1453530ff0506db545cc
Fabio Sciacovelli <fabiosciaco@gmail.com>
2023-05-24 13:24:27 +0200

    implementare validation rules

---
+++ app/Http/Requests/ArticleRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ArticleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+                'title' => 'required|unique:articles|min:5',
+                'subtitle' => 'required|unique:articles|min:5',
+                'body' => 'required|min:10',
+                'image' => 'image|required',
+                'category' => 'required',
+        ];
+    }
+
+    public function message():array{
+        return[
+         
+            'title.required' => 'Devi inserire il titolo dell\' articolo',
+            'title.min' => 'Il titolo deve avere minimo 5 caratteri',
+
+            'subtitle.required' => 'Devi inserire un sottotitolo',
+            'subtitle.min' => 'Il sottotitolo deve avere almeno 5 caratteri',
+
+            'body.required' => 'Devi inserire il corpo dell\' articolo',
+            'body.min' => 'IL corpo del testo deve avere almeno 10 caratteri',
+
+            'image.required' => 'Devi inserire una immagine',
+            
+            'category.required' => 'Devi inserire una categoria',
+        ];
+    }
+}
--- resources/views/article/byUser.blade.php
+++ resources/views/article/byUser.blade.php
@@ -9,7 +9,9 @@
                     <div class="card-body">
                       <h5 class="card-title">{{$article->title}}</h5>
                       <p class="card-text">{{$article->subtitle}}</p>
-                      <p class="small text-muted fst-italic text-capitalize">{{$article->category->name}}</p>
+                      <p class="small text-muted fst-italic text-capitalize">
+                        <a href="{{route('article.byCategory', ['category' => $article->category->id])}}" class="small text-muted fst-italic text-capitalize">{{$article->category->name}}</a>
+                      </p>
                     </div>
                     <div class="card-footer text-muted d-flex justify-content-between align-items-center">
                         Redatto il {{$article->created_at->format('d/m/Y')}} da {{$article->user->name}}
