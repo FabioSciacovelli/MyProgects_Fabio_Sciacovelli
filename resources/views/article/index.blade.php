3574578ddc7637add3db803d347c14b1fd14048b
Fabio Sciacovelli <fabiosciaco@gmail.com>
2023-05-27 13:35:15 +0200

    prove generali

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -37,40 +37,70 @@
               <p class="card-text"><small>Last updated 3 mins ago</small></p>
             </div>
           </div>
-          --}}
+        --}}
 
-          <div class="container my-5">
+            <div class="container my-5">
+                <div class="row justify-content-around">
+        
+                    @foreach($articles as $article)
 
-            
-            <div class="row justify-content-around">
-    
-                @foreach($articles as $article)
-                <div class="col-12 col-md-5 mt-4">
-                    <div class="card rounded-0 text-bg-dark border border-0">
-                        <img src="{{Storage::url($article->image)}}" class="card-img rounded-0" alt="...">
-                        <div class="card-img-overlay d-flex justify-content-end flex-column">
-                            <h5 class="card-title">{{$article->title}}</h5>
-                            <p class="card-text">{{$article->subtitle}}</p>
-                            <p class="card-text small text-muted fst-italic text-capitalize">
-                                <a href="{{route('article.byCategory', ['category' => $article->category->id])}}" class="small text-muted fst-italic text-capitalize">{{$article->category->name}}</a>
-                            </p>
+                        <div class="col-12 col-md-5 mt-4">
+                            <div class="card rounded-0 text-bg-dark border border-0">
+                                <img src="{{Storage::url($article->image)}}" class="card-img rounded-0" alt="...">
+                                <div class="card-img-overlay d-flex justify-content-end flex-column">
+                                    <p class="card-text small text-muted fst-italic text-capitalize">
+                                        <a href="{{route('article.byCategory', ['category' => $article->category->id])}}" class="small text-muted fst-italic text-capitalize">{{$article->category->name}}</a>
+                                    </p>
+                                    <h5 class="card-title">{{$article->title}}</h5>
+                                    <p class="card-text">{{$article->subtitle}}</p>
+                                </div>
+                                
+                            </div>
+                            <div class="card-footer text-bg-dark text-muted d-flex justify-content-center align-items-center">
+                                Redatto il {{$article->created_at->format('d/m/Y')}} da  <a href="{{route('article.byUser', ['user' => $article->user->id])}}" class="small text-muted fst-italic text-capitalize m-2">{{$article->user->name}}</a>
+                                <a href="{{route('article.show', compact('article'))}}" class="ms-5 btn btn-success text-white">Leggi</a>
+                            </div>
                         </div>
-                        
-                    </div>
-                    <div class="card-footer text-bg-dark text-muted d-flex justify-content-center align-items-center">
-                        Redatto il {{$article->created_at->format('d/m/Y')}} da  <a href="{{route('article.byUser', ['user' => $article->user->id])}}" class="small text-muted fst-italic text-capitalize m-2">{{$article->user->name}}</a>
-                        <a href="{{route('article.show', compact('article'))}}" class="ms-5 btn btn-success text-white">Leggi</a>
-                    </div>
-                </div>
-             
-                @endforeach
                 
+                    @endforeach
+                    
+                </div>
             </div>
 
 
 
+            {{-- <div class="container my-5">
+                <div class="row justify-content-around">
         
-    </div>
+                    @foreach($articles as $article)
+
+                        <div class="col-12 col-md-5 mt-4">
+                            <div class="card rounded-0 text-bg-dark border border-0">
+
+                                <img src="{{Storage::url($article->image)}}" class="card-img rounded-0" alt="...">
+                                <div class="card-img-overlay d-flex align-items-end">
+                                    <div class="card-footer text-bg-dark text-muted justify-content-center align-items-center opacity-75">
+                                        <p class="card-text small text-muted fst-italic text-capitalize nopaco">
+                                            <a href="{{route('article.byCategory', ['category' => $article->category->id])}}" class="small text-muted fst-italic text-capitalize nopaco">{{$article->category->name}}</a>
+                                        </p>
+                                        <h5 class="card-title nopaco">{{$article->title}}</h5>
+                                        <p class="card-text nopaco">{{$article->subtitle}}</p>
+                                        Redatto il {{$article->created_at->format('d/m/Y')}} da  <a href="{{route('article.byUser', ['user' => $article->user->id])}}" class="small text-muted fst-italic text-capitalize m-2 nopaco">{{$article->user->name}}</a>
+                                        <a href="{{route('article.show', compact('article'))}}" class="ms-5 btn btn-success text-white">Leggi</a>
+                                    </div>
+                                </div>
+                                
+                            </div>
+
+                        </div>
+                
+                    @endforeach
+                    
+                </div>
+            </div> --}}
+
+
+
     <div class="container mb-5">
         <div class="row justify-content-center">
             <div class="col-12 col-md-2">
