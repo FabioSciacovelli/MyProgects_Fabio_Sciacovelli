eeae1389a33540d2e09907ed3ccdbd37ee79b558
Raffaele Capone <raffaele99roma@gmail.com>
2023-06-01 18:21:20 +0200

    aggiustato frontend

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,5 @@
 <x-layout>
+
   <x-header>
 
     
@@ -12,38 +13,6 @@
             {{session('message')}}
         </div>
     @endif
-{{-- 
-  <div class="container">  BOTTONE INSERISCI ARTICOLO IN HOMEPGAE
-    <div class="row justify-content-center m-4">
-       <div class="col-12 text-center">
-           <a href="{{route('article.create')}}" type="submit" class="btn btn-success">Inserisci il tuo articolo</a>
-       </div>
-    </div>
-  </div> --}}
-
-  {{-- <div class="container my-5">  CARD VECCHIA
-    <div class="row justify-content-around">
-        @foreach($articles as $article)
-        <div class="col-12 col-md-6 mt-4">
-            <div class="card shadow">
-                <img src="{{Storage::url($article->image)}}" class="card-img-top" alt="">
-                <div class="card-body">
-                  <h5 class="card-title">{{$article->title}}</h5>
-                  <p class="card-text">{{$article->subtitle}}</p>
-                  <p class="small text-muted fst-italic text-capitalize">
-                    <a href="{{route('article.byCategory', ['category' => $article->category->id])}}" class="small text-muted fst-italic text-capitalize">{{$article->category->name}}</a>
-                </p>
-                </div>
-                <div class="card-footer text-muted d-flex justify-content-center align-items-center">
-                    Redatto il {{$article->created_at->format('d/m/Y')}} da  <a href="{{route('article.byUser', ['user' => $article->user->id])}}" class="small text-muted fst-italic text-capitalize m-2">{{$article->user->name}}</a>
-                    <a href="{{route('article.show', compact('article'))}}" class="ms-5 btn btn-success text-white">Leggi</a> 
-                </div>
-
-            </div>            
-        </div>
-        @endforeach
-    </div>
-</div> --}}
 
 
 <div class="container my-4">
