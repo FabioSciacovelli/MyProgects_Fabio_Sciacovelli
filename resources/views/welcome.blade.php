a1e9e2537ffe2a891ac26189727b990aee7ea391
Valeriaciav <valeria.ciavola@gmail.com>
2023-06-04 16:07:23 +0200

    modifica sfondo carosello

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -152,22 +152,22 @@
                     <div class="carousel-item active">
                       <img src="/media/carosello3.jpg" class="d-block w-100" alt="...">
                       <div class="carousel-caption d-none d-md-block">
-                        <h2 class="background-blackcustom">La deforestazione è un problema?</h2>
-                        <p class="fs-5 background-blackcustom">Le piante e gli alberi, mediante il processo di fotosintesi clorofilliana, trasformano l'anidride carbonica presente nell'atmosfera in ossigeno. Ne consegue che il disboscamento determina un aumento di CO2 e di conseguenza un acuirsi dell'effetto serra e del riscaldamento globale.</p>
+                        <h2 class="background-blackcustom opacity">La deforestazione è un problema?</h2>
+                        <p class="fs-5 background-blackcustom opacity">Le piante e gli alberi, mediante il processo di fotosintesi clorofilliana, trasformano l'anidride carbonica presente nell'atmosfera in ossigeno. Ne consegue che il disboscamento determina un aumento di CO2 e di conseguenza un acuirsi dell'effetto serra e del riscaldamento globale.</p>
                       </div>
                     </div>
                     <div class="carousel-item ">
                       <img src="/media/spiaggiaplastica.jpg" class="d-block w-100" alt="foto di switch">
                       <div class="carousel-caption d-none d-md-block">
-                        <h2 class="background-blackcustom">Inquinamento marino</h2>
-                        <p class="fs-5 background-blackcustom">Gli effetti dell'inquinamento marino sono molteplici, a partire dalla riduzione della fauna ittica e la perdita di biodiversità, fino all'estinzione di intere specie marine e la cancellazione di una serie di gruppi della catena alimentare.</p>
+                        <h2 class="background-blackcustom opacity">Inquinamento marino</h2>
+                        <p class="fs-5 background-blackcustom opacity">Gli effetti dell'inquinamento marino sono molteplici, a partire dalla riduzione della fauna ittica e la perdita di biodiversità, fino all'estinzione di intere specie marine e la cancellazione di una serie di gruppi della catena alimentare.</p>
                       </div>
                     </div>
                     <div class="carousel-item">
                       <img src="/media/vestitiusati1.jpg" class="d-block w-100" alt="...">
                       <div class="carousel-caption d-none d-md-block">
-                        <h2 class="background-blackcustom">L'inquinamento dell'industria della moda</h2>
-                        <p class="fs-5 background-blackcustom">Secondo un rapporto delle Nazioni Unite, l'industria della moda produce dall'8% al 10% di tutte le emissioni globali di CO2, ovvero tra i 4 e 5 miliardi di tonnellate di anidride carbonica immesse in atmosfera ogni anno.</p>
+                        <h2 class="background-blackcustom opacity">L'inquinamento dell'industria della moda</h2>
+                        <p class="fs-5 background-blackcustom opacity">Secondo un rapporto delle Nazioni Unite, l'industria della moda produce dall'8% al 10% di tutte le emissioni globali di CO2, ovvero tra i 4 e 5 miliardi di tonnellate di anidride carbonica immesse in atmosfera ogni anno.</p>
                       </div>
                     </div>
                     
